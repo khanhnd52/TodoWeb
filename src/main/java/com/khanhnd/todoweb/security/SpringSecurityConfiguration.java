ecd780c1e19a995221c8be27b8f84abca55eeea8
khanhnd52 <khanh.nd5252@gmail.com>
2023-05-22 12:56:09 +0700

    configuring Spring Security to get H2 console working

--- src/main/java/com/khanhnd/todoweb/security/SpringSecurityConfiguration.java
+++ src/main/java/com/khanhnd/todoweb/security/SpringSecurityConfiguration.java
@@ -2,11 +2,15 @@ package com.khanhnd.todoweb.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 import java.util.function.Function;
 
@@ -34,5 +38,19 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new  BCryptPasswordEncoder();
+    }
+
+    //all url are protected
+    //A login form is shown for unauthorized requests
+    //CSRF disable
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        return http.build();
     }
 }
